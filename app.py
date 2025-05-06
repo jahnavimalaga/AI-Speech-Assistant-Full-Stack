6d35c9a1cdba74ed29156968f5d1a19671b97842
Jahnavi <jahnavimalagavalli@gmail.com>
2025-05-05 19:28:22 -0500

    Added more to the web app

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    message = None
     if request.method == 'POST':
         message = request.form.get('user_message')
-    return render_template('index.html', message=message)
+        # Pass message as a query parameter to second page
+        return redirect(url_for('second', message=message))
+    return render_template('index.html')
 
 @app.route('/second')
 def second():
-    return render_template('second.html')
+    message = request.args.get('message', 'No message provided.')
+    return render_template('second.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
